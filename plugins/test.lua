a50721de946e806fef0fdd6711024dd7349ddc8e
3pehrdev <sepehrsalehi0111@gmail.com>
2017-04-19 16:47:56 +0430

    Update test.lua

--- plugins/test.lua
+++ plugins/test.lua
@@ -1,7 +1,7 @@
 local action = function(msg, blocks, ln)
 if blocks[1] == 'ver' then
 local text_start = "*ver 1.2*"
-api.sendMessage(msg.chat.id,text_start, true, true, nil, true, make_menu())
+api.sendMessage(msg.chat.id,text_start, true)
 end
    end
 
